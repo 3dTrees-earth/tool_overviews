92c9431c55ed65a50ecb4fc85b7c1d8ed5e4f504
kgerb <ki-facebook@freenet.de>
2026-01-15 17:39:13 +0100

    viridis as default cmap

--- src/parameters.py
+++ src/parameters.py
@@ -11,7 +11,7 @@ class Parameters(BaseSettings):
     image_width: int = Field(1024, alias=AliasChoices("image-width", "image_width"))
     image_height: int = Field(768, alias=AliasChoices("image-height", "image_height"))
     top_views_deg: int = Field(180, alias=AliasChoices("top-views-deg", "top_views_deg"), description="The angular width of each top view rotation around the LAZ center in degrees.")
-    cmap: str = Field("turbo", description="The colormap to use for the point cloud. All 'cmap' options are available in matplotlib.pyplot.get_cmap()")
+    cmap: str = Field("viridis", description="The colormap to use for the point cloud. All 'cmap' options are available in matplotlib.pyplot.get_cmap()")
     camera_distance: float = Field(35, alias=AliasChoices("camera-distance", "camera_distance"), description="The distance of the camera from the center of the point cloud.")
     output_dir: Path = Field("/out", alias=AliasChoices("output-dir", "output_dir"), description="The directory to save the output images.")
 
